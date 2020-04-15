9c1a447e63061c5a9b0ee606812d6b1d585e0cbc
Eduard Zhukov <35345123+ze-dev@users.noreply.github.com>
2020-04-15 12:02:11 +0300

    Update Asterisk it.py

--- Asterisk it.py
+++ Asterisk it.py
@@ -1,9 +1,22 @@
 '''Asterisk it
 https://www.codewars.com/kata/5888cba35194f7f5a800008b/train/python
+Task
+You are given a function that should insert an asterisk (*) between every pair of even digits in the given input, and return it as a string. If the input is a sequence, concat the elements first as a string.
+
+Input
+The input can be an integer, a string of digits or a sequence containing integers only.
+
+Output
+Return a string.
+
+Examples
+5312708     -->  "531270*8"
+"0000"      -->  "0*0*0*0"
+[1, 4, 64]  -->  "14*6*4"
 '''
-n=8
+n = 8
 n = 5312708; an=  '531270*8'
-#n = 9682135; an = '96*8*2135'
+n = 9682135; an = '96*8*2135'
 n = [1, 4, 64, 68, 67, 23, 1]; an = '14*6*4*6*8*67231'
 
 
